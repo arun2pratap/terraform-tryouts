760856742a066f069d80773bf1882a1656ad6299
ArunPratap Singh <ArunPratap.Singh@iDeaS.com>
2020-05-15 08:54:23 +0530

    * Modular lambda function, per lambda function per operation  * API gateway configured for each lambda function, and javascript function's for the same.  * Using request and response model - to set schema that we accept.  * Use velocity parameter read's path variable and generate param as needed.  ```javascript      passthrough_behavior = "WHEN_NO_TEMPLATES"      request_templates = {        "application/json" :  "#set($inputRoot = $input.path('$'))    {    \"id\": \"$input.params('id')\",    \"message\": \"$inputRoot\"  }"      } ``` * Deploying the same, It does give some exception because terraform was not able to deploy that in the right sequence, had to give depends_on = [] tag to control that behaviour.

--- aws/terraform-labda-dyanmodb-with-api-gateway/main.tf
+++ aws/terraform-labda-dyanmodb-with-api-gateway/main.tf
@@ -24,31 +24,90 @@ EOF
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_attach_policy_basicExecutionRole" {
-  role       = aws_iam_role.role.name
+  role = aws_iam_role.role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_attach_policy_dynamoDB" {
-  role       = aws_iam_role.role.name
+  role = aws_iam_role.role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
 }
-
+# lambda function conversation get
 data "archive_file" "lambda_zip" {
-  type        = "zip"
-  source_file = "lambda/index.js"
-  output_path = "lambda_function.zip"
+  type = "zip"
+  source_file = "lambda/conversation_get.js"
+  output_path = "lambda_function_conversation_get.zip"
 }
 
-resource "aws_lambda_function" "lambda" {
-  function_name    = "lets-chat-API_03"
-  filename         = "lambda_function.zip"
-  handler          = "index.handler"
-  role             = aws_iam_role.role.arn
-  runtime          = "nodejs12.x"
-  source_code_hash = filebase64sha256("lambda/index.js")
+resource "aws_lambda_function" "lambda_conversation_get" {
+  function_name = "Chat-Conversation-Get"
+  filename = "lambda_function_conversation_get.zip"
+  handler = "conversation_get.handler"
+  role = aws_iam_role.role.arn
+  runtime = "nodejs12.x"
+  source_code_hash = filebase64sha256("lambda/conversation_get.js")
 }
 
+resource "aws_lambda_permission" "apigw_lambda" {
+  statement_id = "AllowExecutionFromAPIGateway"
+  action = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda_conversation_get.function_name
+  principal = "apigateway.amazonaws.com"
+  source_arn = "${aws_api_gateway_rest_api.api.execution_arn}/*/*/*"
+  # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
+  #  source_arn = "arn:aws:execute-api:${var.region}:${var.accountId}:${aws_api_gateway_rest_api.api.id}/*/${aws_api_gateway_method.method.http_method}${aws_api_gateway_resource.resource.path}"
+}
+# lambda function message GET
 
+data "archive_file" "lambda_zip_message_get" {
+  type = "zip"
+  source_file = "lambda/messages-get.js"
+  output_path = "lambda_function_messages-get.zip"
+}
+
+resource "aws_lambda_function" "lambda_message_get" {
+  function_name = "Chat-Messages-GET"
+  filename = "lambda_function_messages-get.zip"
+  handler = "messages-get.handler"
+  role = aws_iam_role.role.arn
+  runtime = "nodejs12.x"
+  source_code_hash = filebase64sha256("lambda/messages-get.js")
+}
+resource "aws_lambda_permission" "apigw_lambda_message_get" {
+  statement_id = "AllowExecutionFromAPIGateway"
+  action = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda_message_get.function_name
+  principal = "apigateway.amazonaws.com"
+  source_arn = "${aws_api_gateway_rest_api.api.execution_arn}/*/*/*"
+  # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
+  #  source_arn = "arn:aws:execute-api:${var.region}:${var.accountId}:${aws_api_gateway_rest_api.api.id}/*/${aws_api_gateway_method.method.http_method}${aws_api_gateway_resource.resource.path}"
+}
+# lambda function post
+
+data "archive_file" "lambda_zip_message_post" {
+  type = "zip"
+  source_file = "lambda/messages-post.js"
+  output_path = "lambda_function_messages-post.zip"
+}
+
+resource "aws_lambda_function" "lambda_message_post" {
+  function_name = "Chat-Messages-POST"
+  filename = "lambda_function_messages-post.zip"
+  handler = "messages-post.handler"
+  role = aws_iam_role.role.arn
+  runtime = "nodejs12.x"
+  source_code_hash = filebase64sha256("lambda/messages-post.js")
+}
+
+resource "aws_lambda_permission" "apigw_lambda_message_post" {
+  statement_id = "AllowExecutionFromAPIGateway"
+  action = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda_message_post.function_name
+  principal = "apigateway.amazonaws.com"
+  source_arn = "${aws_api_gateway_rest_api.api.execution_arn}/*/*/*"
+  # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
+  #  source_arn = "arn:aws:execute-api:${var.region}:${var.accountId}:${aws_api_gateway_rest_api.api.id}/*/${aws_api_gateway_method.method.http_method}${aws_api_gateway_resource.resource.path}"
+}
 # --- end lamdba policy/role for  -------
 
 # --- start API gateway ---------------
@@ -56,27 +115,27 @@ resource "aws_lambda_function" "lambda" {
 # https://www.terraform.io/docs/providers/aws/r/api_gateway_integration.html
 
 resource "aws_api_gateway_rest_api" "api" {
-  name        = "00lambdaAPI"
+  name = "letsChatAPI"
   description = "Lets Chat Lambda API"
   # Valid values: EDGE, REGIONAL or PRIVATE
   endpoint_configuration {
     types = [
-    "REGIONAL"]
+      "REGIONAL"]
   }
 }
 resource "aws_api_gateway_resource" "resource" {
   path_part = "conversations"
   # To define it's a proxy resource
-  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+  parent_id = aws_api_gateway_rest_api.api.root_resource_id
   rest_api_id = aws_api_gateway_rest_api.api.id
 }
 # --- lambda integration -------------
 
 #https://www.terraform.io/docs/providers/aws/r/api_gateway_method.html
 resource "aws_api_gateway_method" "method" {
-  rest_api_id   = aws_api_gateway_rest_api.api.id
-  resource_id   = aws_api_gateway_resource.resource.id
-  http_method   = "GET"
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.resource.id
+  http_method = "GET"
   authorization = "NONE"
 }
 
@@ -90,33 +149,13 @@ resource "aws_api_gateway_integration" "integration" {
     "application/json" : "{\"statusCode\": 200}"
   }
   type = "AWS"
-  uri  = aws_lambda_function.lambda.invoke_arn
+  uri = aws_lambda_function.lambda_conversation_get.invoke_arn
 }
 
-resource "aws_lambda_permission" "apigw_lambda" {
-  statement_id  = "AllowExecutionFromAPIGateway"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.lambda.function_name
-  principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/*/*/*"
-  # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
-  #  source_arn = "arn:aws:execute-api:${var.region}:${var.accountId}:${aws_api_gateway_rest_api.api.id}/*/${aws_api_gateway_method.method.http_method}${aws_api_gateway_resource.resource.path}"
-}
-
-resource "aws_api_gateway_method_response" "options_200" {
+resource "aws_api_gateway_model" "converstaionList" {
   rest_api_id = aws_api_gateway_rest_api.api.id
-  resource_id = aws_api_gateway_resource.resource.id
-  http_method = aws_api_gateway_method.method.http_method
-  status_code = "200"
-  response_models = {
-    "application/json" = aws_api_gateway_model.MyDemoModel.name
-  }
-}
-
-resource "aws_api_gateway_model" "MyDemoModel" {
-  rest_api_id  = aws_api_gateway_rest_api.api.id
-  name         = "ConverstationList"
-  description  = "a JSON schema"
+  name = "ConverstationList"
+  description = "a JSON schema"
   content_type = "application/json"
 
   schema = <<EOF
@@ -144,6 +183,16 @@ resource "aws_api_gateway_model" "MyDemoModel" {
 EOF
 }
 
+resource "aws_api_gateway_method_response" "options_200" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.resource.id
+  http_method = aws_api_gateway_method.method.http_method
+  status_code = "200"
+  response_models = {
+    "application/json" = aws_api_gateway_model.converstaionList.name
+  }
+}
+
 resource "aws_api_gateway_integration_response" "options_integration_response" {
   rest_api_id = aws_api_gateway_rest_api.api.id
   resource_id = aws_api_gateway_resource.resource.id
@@ -155,31 +204,169 @@ resource "aws_api_gateway_integration_response" "options_integration_response" {
 
 resource "aws_api_gateway_resource" "resource_conv" {
   path_part = "{id}"
-  parent_id   = aws_api_gateway_resource.resource.id
+  parent_id = aws_api_gateway_resource.resource.id
   rest_api_id = aws_api_gateway_rest_api.api.id
 }
 
+# Get API Gateway flow
+
 resource "aws_api_gateway_method" "method_conv_GET" {
-  rest_api_id   = aws_api_gateway_rest_api.api.id
-  resource_id   = aws_api_gateway_resource.resource_conv.id
-  http_method   = "GET"
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.resource_conv.id
+  http_method = "GET"
   authorization = "NONE"
 }
+
+resource "aws_api_gateway_integration" "integration_conv_get" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.resource_conv.id
+  http_method = aws_api_gateway_method.method_conv_GET.http_method
+  integration_http_method = "POST"
+  passthrough_behavior = "WHEN_NO_TEMPLATES"
+  # velocity parameter read's path variable and generate param as needed.
+  request_templates = {
+    "application/json": "#set($inputRoot = $input.path('$'))    {    \"id\": \"$input.params('id')\"  }"
+  }
+//  request_templates = {
+//    "application/json" : "{\"statusCode\": 200}"
+//  }
+  type = "AWS"
+  uri = aws_lambda_function.lambda_message_get.invoke_arn
+}
+
+resource "aws_api_gateway_model" "conversations" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  name = "Converstation"
+  description = "a JSON schema"
+  content_type = "application/json"
+  schema = <<EOF
+{
+  "type": "object",
+  "properties": {
+    "id": {
+      "type": "string"
+    },
+    "participants": {
+      "type": "array",
+      "items": {
+        "type": "string"
+      }
+    },
+    "last": {
+      "type": "number",
+      "format": "utc-millisec"
+    },
+    "messages": {
+      "type": "array",
+      "items": {
+        "type": "object",
+        "properties": {
+          "sender": {
+            "type": "string"
+          },
+          "time": {
+            "type": "number",
+            "format": "utc-millisec"
+          },
+          "message": {
+            "type": "string"
+          }
+        }
+      }
+    }
+  }
+}
+EOF
+}
+
+resource "aws_api_gateway_method_response" "response_conv_get" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.resource_conv.id
+  http_method = aws_api_gateway_method.method_conv_GET.http_method
+  status_code = "200"
+  response_models = {
+    "application/json" = aws_api_gateway_model.conversations.name
+  }
+}
+
+resource "aws_api_gateway_integration_response" "integration_res_conv_get" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.resource_conv.id
+  http_method = aws_api_gateway_method.method_conv_GET.http_method
+  status_code = aws_api_gateway_method_response.response_conv_get.status_code
+}
+### post API gateway flow
+
+resource "aws_api_gateway_model" "newMessage" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  name = "NewMessage"
+  description = "a JSON schema"
+  content_type = "application/json"
+  schema = <<EOF
+{
+  "type": "string"
+}
+EOF
+}
+
 resource "aws_api_gateway_method" "method_conv_POST" {
-  rest_api_id   = aws_api_gateway_rest_api.api.id
-  resource_id   = aws_api_gateway_resource.resource_conv.id
-  http_method   = "POST"
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.resource_conv.id
+  http_method = "POST"
   authorization = "NONE"
+  request_models = {
+    "application/json" : aws_api_gateway_model.newMessage.name
+  }
+}
+resource "aws_api_gateway_integration" "integration_conv_post" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.resource_conv.id
+  http_method = aws_api_gateway_method.method_conv_POST.http_method
+  integration_http_method = "POST"
+  # velocity parameter read's path variable and generate param as needed.
+  passthrough_behavior = "WHEN_NO_TEMPLATES"
+  request_templates = {
+    "application/json" :  "#set($inputRoot = $input.path('$'))    {    \"id\": \"$input.params('id')\",    \"message\": \"$inputRoot\"  }"
+  }
+  type = "AWS"
+  uri = aws_lambda_function.lambda_message_post.invoke_arn
+}
+
+resource "aws_api_gateway_method_response" "response_conv_post" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.resource_conv.id
+  http_method = aws_api_gateway_method.method_conv_POST.http_method
+  status_code = 204
+  response_models = {
+    "application/json" = "Empty"
+  }
+}
+
+resource "aws_api_gateway_integration_response" "integration_res_conv_post" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.resource_conv.id
+  http_method = aws_api_gateway_method.method_conv_POST.http_method
+  status_code = aws_api_gateway_method_response.response_conv_post.status_code
 }
 ##############
 
-# --- Mock Integration for CORS
+# --- Mock Integration for Enable CORS for resource /converstations
 
 resource "aws_api_gateway_method" "method_cors" {
   rest_api_id = aws_api_gateway_rest_api.api.id
   resource_id = aws_api_gateway_resource.resource.id
   http_method = "OPTIONS"
   authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "integration_cors" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.resource.id
+  http_method = aws_api_gateway_method.method_cors.http_method
+  type = "MOCK"
+  request_templates = {
+    "application/json": "{\"statusCode\": 200}"
+  }
 }
 
 resource "aws_api_gateway_method_response" "response_cors" {
@@ -197,16 +384,6 @@ resource "aws_api_gateway_method_response" "response_cors" {
   }
 }
 
-resource "aws_api_gateway_integration" "integration_cors" {
-  rest_api_id = aws_api_gateway_rest_api.api.id
-  resource_id = aws_api_gateway_resource.resource.id
-  http_method = aws_api_gateway_method.method_cors.http_method
-  type = "MOCK"
-  request_templates = {
-    "application/json": "{\"statusCode\": 200}"
-  }
-}
-
 resource "aws_api_gateway_integration_response" "integration_res_cors" {
   rest_api_id = aws_api_gateway_rest_api.api.id
   resource_id = aws_api_gateway_resource.resource.id
@@ -219,15 +396,81 @@ resource "aws_api_gateway_integration_response" "integration_res_cors" {
   }
 }
 ############
+
+# --- Mock Integration for Enable CORS for resource_conv /converstations/{id}
+
+resource "aws_api_gateway_method" "method_conv_cors" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.resource_conv.id
+  http_method = "OPTIONS"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "integration_conv_cors" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.resource_conv.id
+  http_method = aws_api_gateway_method.method_conv_cors.http_method
+  type = "MOCK"
+  request_templates = {
+    "application/json": "{\"statusCode\": 200}"
+  }
+}
+
+resource "aws_api_gateway_method_response" "response_conv_cors" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.resource_conv.id
+  http_method = aws_api_gateway_method.method_conv_cors.http_method
+  status_code = "200"
+  response_models = {
+    "application/json" = "Empty"
+  }
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true,
+    "method.response.header.Access-Control-Allow-Methods" = true,
+    "method.response.header.Access-Control-Allow-Origin" = true
+  }
+}
+
+resource "aws_api_gateway_integration_response" "integration_res_conv_cors" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.resource_conv.id
+  http_method = aws_api_gateway_method.method_conv_cors.http_method
+  status_code = aws_api_gateway_method_response.response_conv_cors.status_code
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST,PUT'",
+    "method.response.header.Access-Control-Allow-Origin" = "'*'"
+  }
+}
+############
+
 # dploy lambda funciton
-//resource "aws_api_gateway_deployment" "deployment" {
-//  rest_api_id = aws_api_gateway_rest_api.api.id
-//  stage_name  = "test"
-//  lifecycle {
-//    create_before_destroy = true
-//  }
-//  depends_on = [aws_api_gateway_resource.resource ]
-//}
+resource "aws_api_gateway_deployment" "deployment" {
+    depends_on = [
+      aws_api_gateway_resource.resource,
+      aws_api_gateway_resource.resource_conv,
+      aws_api_gateway_method.method,
+      aws_api_gateway_method.method_conv_GET,
+      aws_api_gateway_method.method_conv_POST,
+      aws_api_gateway_method.method_cors,
+      aws_api_gateway_method.method_conv_cors,
+      aws_api_gateway_method_response.options_200,
+      aws_api_gateway_method_response.response_cors,
+      aws_api_gateway_method_response.response_conv_cors,
+      aws_api_gateway_method_response.response_conv_get,
+      aws_api_gateway_method_response.response_conv_post,
+      aws_api_gateway_integration.integration,
+      aws_api_gateway_integration.integration_cors,
+      aws_api_gateway_integration.integration_conv_cors,
+      aws_api_gateway_integration_response.options_integration_response,
+      aws_api_gateway_integration_response.integration_res_conv_get,
+      aws_api_gateway_integration_response.integration_res_conv_post,
+      aws_api_gateway_integration_response.integration_res_cors,
+      aws_api_gateway_integration_response.integration_res_conv_cors
+    ]
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  stage_name = "Dev"
+}
 
 ###### dynamo db  tables for lambda to read from UNCOMMENT the code below
 /*
